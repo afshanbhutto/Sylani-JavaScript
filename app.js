354f2de667b34871570ca3ed691051d8d856916b
Afshan Bhutto <64063271+afshanbhutto@users.noreply.github.com>
2020-06-14 15:39:50 +0500

    added two files

---
+++ app.js
@@ -0,0 +1,143 @@
+/// chapter #1 
+
+
+
+//1.
+alert("Hello this is my first Javascript Program");
+
+//2.
+
+alert("Error! Please enter a valid password.");
+
+
+//3.
+
+alert("Welcome to JS Land.......\n Happy Coding!");
+
+
+//4.
+alert("Welcome to JS Land  ");
+
+alert("Happy Coding!\n Prevent this Page from creating dialogs. ");
+
+//5.
+
+console.log("Hello i can run JS through my web broswer's console")
+
+
+
+
+
+//chapter# 02
+
+
+//1.
+var username;
+
+//2.
+var myName="Afshan Bhutto";
+
+//3.
+var title ="Hello world";
+
+alert(title);
+
+
+
+//4.
+var name="Haider"
+
+alert(name);
+var age="16"
+alert(age +"years old");
+var course="Certified Mobile Application Development"
+alert(course);
+
+
+//5.
+
+var name="PIZZA"
+var msg=""
+for (var i=name.length; i>=1;i--)
+
+{
+
+msg+=name.slice(0,i)+"\n"
+
+}
+
+alert(msg)
+
+//6.
+
+var email="afshanbhutto6@gmail.com";
+alert("my email adress is "+email)
+
+
+//7.
+
+var book=" A Smater way to Learn JavaScript"
+alert("I am trying to Learn from the Book"+ book)
+
+
+//8. 
+document.write("Yah! I can write HTML content through JavaScript")
+
+
+//9.
+
+alert("______________۩۞۩______________")
+document.write("______________۩۞۩______________")
+
+//chapter# 03
+
+//1.
+var age=19;
+
+alert("i am "+ age +" year old");
+
+//2.
+
+var counter=0;
+var newnum=counter+1;
+alert("you have visited this site"+newnum+"times")
+
+//3.
+var birthYear=2001;
+document.write("My birth year is "+birthYear+"\n Data type of my declaered varibale is number ")
+
+//4.
+var VistorsName="John Doe"
+var Producttitle="T-Shirts"
+var qantity=5
+document.write(VistorsName+" ordered "+qantity+Producttitle+"(s)" +" on XYZ Clothing Store")
+
+
+//chapter# 04
+
+//1.
+var a,b,c;
+
+//2 legal
+var ali;
+var _ajdf;
+var oiu1;
+var oia;
+var u712;
+
+//illegal 
+
+// var var;
+// var 1na;
+// var 123;
+// var -hah;
+// var 9ooo9;
+
+//3.
+
+Document.write("Rules for naming JS variables \n"+"Variable names can only contain numbers, $, characters For example $my_1stVariable  \n");
+Document.write("Variables must begin with a letter , $ , or _. For example $name, _name or name\n"+"Variable names are case senstive\n")
+Document.write("Variable names should not be JS keywords")
+ 
+
+//chapter# 05
